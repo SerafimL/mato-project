de2e4556c3f90d68436e1a4598227c0ede58b180
Lucas Serafim <lucasserafim.salesforce@gmail.com>
2024-03-25 18:55:45 -0300

    more initial preparation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-import HeaderBar from './components/headerBar/headerBar';
 
 function App() {
   return (
     <div className="App">
-      <HeaderBar>[NOME do SITE]</HeaderBar>
     </div>
   );
 }
